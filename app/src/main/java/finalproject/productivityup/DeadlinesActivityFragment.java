5f46c14012cd58acaf67957333bbafb9c3cd49ca
fruitspunchs <zargon234@gmail.com>
2015-11-25 15:35:59 +0800

    Initial commit

---
+++ app/src/main/java/finalproject/productivityup/DeadlinesActivityFragment.java
@@ -0,0 +1,22 @@
+package finalproject.productivityup;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class DeadlinesActivityFragment extends Fragment {
+
+    public DeadlinesActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_deadlines, container, false);
+    }
+}
---
+++ app/src/main/java/finalproject/productivityup/PomodoroTimerActivityFragment.java
@@ -0,0 +1,22 @@
+package finalproject.productivityup;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PomodoroTimerActivityFragment extends Fragment {
+
+    public PomodoroTimerActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_pomodoro_timer, container, false);
+    }
+}
