b6b2aa4c15026ab15d8764006a320b91d35dde07
fruitspunchs <zargon234@gmail.com>
2015-12-16 04:23:51 +0800

    feat: add switch to date/time picker

--- app/src/main/java/finalproject/productivityup/AddDeadlineActivity.java
+++ app/src/main/java/finalproject/productivityup/AddDeadlineActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.ImageButton;
+import android.widget.TimePicker;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -14,9 +18,37 @@ public class AddDeadlineActivity extends AppCompatActivity {
     @Bind(R.id.add_deadline_done_fab)
     FloatingActionButton doneFab;
 
+    @Bind(R.id.add_deadline_calendar_view)
+    CalendarView calendarView;
+
+    @Bind(R.id.add_deadline_time_picker)
+    TimePicker timePicker;
+
+    @Bind(R.id.add_deadline_date_time_image_button)
+    ImageButton dateTimeButton;
+    private int mode = MODE.DATE;
+
     @OnClick(R.id.add_deadline_done_fab)
     void clickDoneFab() {
         finish();
+    }
+
+    @OnClick(R.id.add_deadline_date_time_image_button)
+    void clickDateTimeButton() {
+        switch (mode) {
+            case MODE.DATE:
+                mode = MODE.TIME;
+                dateTimeButton.setImageResource(R.drawable.ic_event_white_48dp);
+                calendarView.setVisibility(View.INVISIBLE);
+                timePicker.setVisibility(View.VISIBLE);
+                break;
+            case MODE.TIME:
+                mode = MODE.DATE;
+                dateTimeButton.setImageResource(R.drawable.ic_alarm_white_48dp);
+                calendarView.setVisibility(View.VISIBLE);
+                timePicker.setVisibility(View.INVISIBLE);
+                break;
+        }
     }
 
     @Override
@@ -28,6 +60,11 @@ public class AddDeadlineActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         ButterKnife.bind(this);
+    }
+
+    private interface MODE {
+        int DATE = 0;
+        int TIME = 1;
     }
 
 }
