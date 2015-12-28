ba8869643b9438f9a9d6f2a08ec169d7fa498e95
fruitspunchs <zargon234@gmail.com>
2015-12-28 15:35:49 +0800

    feat: show next deadline task

--- app/src/main/java/finalproject/productivityup/MainActivity.java
+++ app/src/main/java/finalproject/productivityup/MainActivity.java
@@ -1,9 +1,14 @@
 package finalproject.productivityup;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
@@ -11,15 +16,21 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.Calendar;
 import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import finalproject.productivityup.adapter.OverviewDeadlinesCursorAdapter;
+import finalproject.productivityup.data.DeadlineTasksColumns;
+import finalproject.productivityup.data.ProductivityProvider;
+import finalproject.productivityup.libs.LinearLayoutManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+    private static final int DEADLINE_TASKS_CURSOR_LOADER_ID = 0;
+    private static final int NEXT_DEADLINE_CURSOR_LOADER_ID = 1;
     private final String LOG_TAG = this.getClass().getSimpleName();
-
     @Bind(R.id.overview_card_pomodoro_timer)
     CardView mPomodoroTimerCard;
     @Bind(R.id.overview_card_ultradian_rhythm)
@@ -32,10 +43,15 @@ public class MainActivity extends AppCompatActivity {
     CardView mAccountabilityChartCard;
     @Bind(R.id.overview_card_productivity_quiz)
     CardView mProductivityQuizCard;
-
     @Bind({R.id.deadlines_title_text_view, R.id.accountability_chart_title1_text_view, R.id.accountability_chart_title2_text_view, R.id.agenda_title_text_view, R.id.ultradian_rhythm_title_text_view, R.id.pomodoro_timer_title_text_view})
-    List<TextView> cardTitles;
-    boolean isShowingCardTitles = true;
+    List<TextView> mCardTitles;
+    boolean mIsShowingCardTitles = true;
+    @Bind(R.id.deadlines_task_text_view)
+    RecyclerView mDeadlinesTaskRecyclerView;
+    @Bind(R.id.deadlines_date_text_view)
+    TextView mDeadlinesDate;
+    OverviewDeadlinesCursorAdapter mOverviewDeadlinesCursorAdapter;
+    private long nextDeadlineUnixTime = -1;
 
     @OnClick(R.id.overview_card_pomodoro_timer)
     void clickPomodoroCard() {
@@ -82,6 +98,18 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         ButterKnife.bind(this);
+
+        getSupportLoaderManager().initLoader(DEADLINE_TASKS_CURSOR_LOADER_ID, null, this);
+        mDeadlinesTaskRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mOverviewDeadlinesCursorAdapter = new OverviewDeadlinesCursorAdapter(this, null);
+        mDeadlinesTaskRecyclerView.setAdapter(mOverviewDeadlinesCursorAdapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "Restarting loader");
+        getSupportLoaderManager().restartLoader(DEADLINE_TASKS_CURSOR_LOADER_ID, null, this);
     }
 
     @Override
@@ -102,23 +130,94 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         } else if (id == R.id.action_toggle_card_title) {
-            if (isShowingCardTitles) {
+            if (mIsShowingCardTitles) {
                 Log.d(LOG_TAG, "Hiding card titles");
-                for (TextView textView : cardTitles) {
+                for (TextView textView : mCardTitles) {
                     textView.setVisibility(View.GONE);
                 }
                 item.setIcon(R.drawable.ic_show_card_title);
-                isShowingCardTitles = false;
+                mIsShowingCardTitles = false;
             } else {
                 Log.d(LOG_TAG, "Showing card titles");
-                for (TextView textView : cardTitles) {
+                for (TextView textView : mCardTitles) {
                     textView.setVisibility(View.VISIBLE);
                 }
                 item.setIcon(R.drawable.ic_hide_card_title);
-                isShowingCardTitles = true;
+                mIsShowingCardTitles = true;
             }
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        if (id == DEADLINE_TASKS_CURSOR_LOADER_ID) {
+            Calendar currentTime = Calendar.getInstance();
+            long currentTimeInSeconds = currentTime.getTimeInMillis() / 1000;
+            String[] selectionArgs = {String.valueOf(currentTimeInSeconds)};
+
+            return new CursorLoader(this, ProductivityProvider.DeadlineTasks.CONTENT_URI,
+                    null,
+                    DeadlineTasksColumns.TIME + " >= ?",
+                    selectionArgs,
+                    DeadlineTasksColumns.TIME + " ASC");
+        } else if (id == NEXT_DEADLINE_CURSOR_LOADER_ID) {
+            String[] selectionArgs = {String.valueOf(nextDeadlineUnixTime)};
+
+            return new CursorLoader(this, ProductivityProvider.DeadlineTasks.CONTENT_URI,
+                    null,
+                    DeadlineTasksColumns.TIME + " = ?",
+                    selectionArgs,
+                    null);
+        }
+
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+        if (loader.getId() == NEXT_DEADLINE_CURSOR_LOADER_ID) {
+            mOverviewDeadlinesCursorAdapter.swapCursor(data);
+        } else if (loader.getId() == DEADLINE_TASKS_CURSOR_LOADER_ID) {
+            if (data.moveToNext()) {
+                nextDeadlineUnixTime = data.getLong(data.getColumnIndex(DeadlineTasksColumns.TIME));
+                getSupportLoaderManager().restartLoader(NEXT_DEADLINE_CURSOR_LOADER_ID, null, this);
+            }
+        }
+
+
+//        if (data.moveToNext()) {
+//            Long date = data.getLong(data.getColumnIndex(DeadlineTasksColumns.TIME));
+//            Long nextDate;
+//            String task = "";
+//            int ctr = 0;
+//
+//            do {
+//
+//                if (ctr > 0) {
+//                    task += "\n";
+//                }
+//                ctr++;
+//
+//                task += data.getString(data.getColumnIndex(DeadlineTasksColumns.TASK));
+//
+//                if (data.moveToNext()) {
+//                    nextDate = data.getLong(data.getColumnIndex(DeadlineTasksColumns.TIME));
+//                } else {
+//                    nextDate = -1L;
+//                }
+//
+//            } while (date.equals(nextDate));
+//
+//            mDeadlinesTaskRecyclerView.setText(task);
+//            mDeadlinesDate.setText(String.valueOf(date));
+//        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mOverviewDeadlinesCursorAdapter.swapCursor(null);
     }
 }
---
+++ app/src/main/java/finalproject/productivityup/adapter/OverviewDeadlinesCursorAdapter.java
@@ -0,0 +1,51 @@
+package finalproject.productivityup.adapter;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import finalproject.productivityup.R;
+import finalproject.productivityup.data.DeadlineTasksColumns;
+
+/**
+ * Created by User on 12/18/2015.
+ */
+public class OverviewDeadlinesCursorAdapter extends CursorRecyclerViewAdapter<OverviewDeadlinesCursorAdapter.ViewHolder> {
+    ViewHolder mVh;
+    private Context mContext;
+
+    public OverviewDeadlinesCursorAdapter(Context context, Cursor cursor) {
+        super(context, cursor);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, Cursor cursor) {
+        viewHolder.mTaskTextView.setText(cursor.getString(cursor.getColumnIndex(DeadlineTasksColumns.TASK)));
+    }
+
+    public void setContext(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_overview_deadlines_list, parent, false);
+        ViewHolder vh = new ViewHolder(itemView);
+        mVh = vh;
+        return vh;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView mTaskTextView;
+
+        public ViewHolder(View view) {
+            super(view);
+            mTaskTextView = (TextView) view.findViewById(R.id.deadlines_task_text_view);
+        }
+    }
+}
