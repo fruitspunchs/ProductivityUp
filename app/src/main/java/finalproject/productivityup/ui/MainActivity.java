49dc028b095cc00a2fa5e65fa65cf728c4ab58b9
fruitspunchs <zargon234@gmail.com>
2016-03-05 02:35:20 +0800

    docs: update todo

--- app/src/main/java/finalproject/productivityup/ui/MainActivity.java
+++ app/src/main/java/finalproject/productivityup/ui/MainActivity.java
@@ -27,6 +27,9 @@ import finalproject.productivityup.ui.agenda.AgendaActivity;
 import finalproject.productivityup.ui.deadlines.DeadlinesActivity;
 import finalproject.productivityup.ui.deadlines.DeadlinesActivityFragment;
 
+// TODO: 3/5/2016 move db queries to AsyncTask
+// TODO: 3/5/2016 create widget
+
 // TODO: 1/24/2016 ask if deadline was met, or reschedule or archive
 // TODO: 1/24/2016 add animations to card resize
 public class MainActivity extends AppCompatActivity {
